228d4c8e2475c761d678871d6c6b2ba51516ec47
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-10-14 23:18:17 +0530

    Create Bresenham's_circle_mudpoint_algo.cpp

---
+++ Bresenham's_circle_mudpoint_algo.cpp
@@ -0,0 +1,44 @@
+# Bresenham's_circle_mudpoint_algorithm
+
+#include<graphics.h>
+#include<conio.h>
+#include<iostream.h>
+
+void main()
+{
+int h,k,x,y,r,d;
+int gd= DETECT,gm;
+initgraph(&gd,&gm," ");
+cout<<"enter the value of centre coodinate"<<endl;
+cin>>h>>k;
+cout<<"enter the value of radius"<<endl;
+cin>>r;
+x=0;
+y=r;
+d=1-r;
+do
+{
+   putpixel(h+x,k+y,4);
+   putpixel(h+x,k-y,4);
+   putpixel(h-x,k+y,4);
+   putpixel(h-x,k-y,4);
+   putpixel(h+y,k+x,4);
+   putpixel(h+y,k-x,4);
+   putpixel(h-y,k+x,4);
+   putpixel(h-y,k-x,4);
+   if(d<0)
+   {
+      x=x+1;
+      y=y;
+      d=d+(2*x)+3;
+   }
+   else
+   {
+      x=x+1;
+      y=y-1;
+      d=d+2*(x-y)+5;
+   }
+}while(x<y);
+getch();
+closegraph();
+}
