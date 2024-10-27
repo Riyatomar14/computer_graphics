7118b80f11b1556d3c1145881b65c6b6436a5d8a
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-10-27 06:01:35 +0530

    Update Bresenham's_circle_midpoint_algo.cpp

--- Bresenham's_circle_midpoint_algo.cpp
+++ Bresenham's_circle_midpoint_algo.cpp
@@ -1,51 +1,41 @@
-#include <graphics.h> 
-#include <conio.h>
-#include <iostream>
+#include<graphics.h>
+#include<conio.h>
+#include<iostream.h>
 
-using namespace std;
-
-void drawCircleBresenham(int h, int k, int r) {
-    int x = 0, y = r;
-    int d = 1 - r;
-
-    do {
-        putpixel(h + x, k + y, 4);  // Draw 8 symmetric points
-        putpixel(h + x, k - y, 4);
-        putpixel(h - x, k + y, 4);
-        putpixel(h - x, k - y, 4);
-        putpixel(h + y, k + x, 4);
-        putpixel(h + y, k - x, 4);
-        putpixel(h - y, k + x, 4);
-        putpixel(h - y, k - x, 4);
-
-        if (d < 0) {
-            x++;
-            d += 2 * x + 3;
-        } else {
-            x++;
-            y--;
-            d += 2 * (x - y) + 5;
-        }
-    } while (x < y);
-}
-
-int main() {
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");  // Graphics initialization
-
-    int h, k, r;
-    cout << "Enter the center coordinates (h, k): ";
-    cin >> h >> k;
-    cout << "Enter the radius: ";
-    cin >> r;
-
-    drawCircleBresenham(h, k, r);  // Call function to draw the circle
-
-    getch();
-    closegraph();  // Close the graphics window
-    return 0;
-}
-
+void main()
+{
+int h,k,x,y,r,d;
+int gd= DETECT,gm;
+initgraph(&gd,&gm," ");
+cout<<"enter the value of centre coodinate"<<endl;
+cin>>h>>k;
+cout<<"enter the value of radius"<<endl;
+cin>>r;
+x=0;
+y=r;
+d=1-r;
+do
+{
+   putpixel(h+x,k+y,4);
+   putpixel(h+x,k-y,4);
+   putpixel(h-x,k+y,4);
+   putpixel(h-x,k-y,4);
+   putpixel(h+y,k+x,4);
+   putpixel(h+y,k-x,4);
+   putpixel(h-y,k+x,4);
+   putpixel(h-y,k-x,4);
+   if(d<0)
+   {
+      x=x+1;
+      y=y;
+      d=d+(2*x)+3;
+   }
+   else
+   {
+      x=x+1;
+      y=y-1;
+      d=d+2*(x-y)+5;
+   }
 }while(x<y);
 getch();
 closegraph();
